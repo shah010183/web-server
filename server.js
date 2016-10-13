95785b501bb0a7f04d10e5aa48d3687ff648bdb1
Manan Shah <shah010183@gmail.com>
2016-10-13 14:09:44 -0700

    seperate middlware code from server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require('express');
+const util = require('util')
 var app = express(); //express app
 var port = 3000;
 
-app.get('/',function(req, res){
-  res.send('Hello Express!');
-});
+var middleware = require('./middleware.js');
+//moved all middleware code to this file
 
-app.get('/about',function(req, res){
+// inspect middleware
+console.log("middleware: " + util.inspect(middleware, false, null));
+
+// for all pages
+// app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+// for specific pages add it as a second arguement to app.get
+
+app.get('/about',middleware.requireAuthentication, function(req, res){
   res.send('About us');
 });
 
@@ -16,5 +24,5 @@ console.log("Web server address: " + __dirname);
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function(){
-  console.log('Express server started (port : '+ port +')...');
+  console.log('Express server started (port : '+ port +')...!');
 });
